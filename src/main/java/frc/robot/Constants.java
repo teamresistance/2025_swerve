89333c12bc453ccbf03874d4cc91b0fd5421c628
Aryan <52215513+importly@users.noreply.github.com>
2024-06-16 17:57:07 -0400

    complete working swerve drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,58 +24,55 @@ import edu.wpi.first.math.util.Units;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-
-  // Modes
-  public static final Mode currentMode = Mode.REAL;
-
-  public enum Mode {
-    /** Running on a real robot. */
-    REAL,
-
-    /** Running a physics simulator. */
-    SIM,
-
-    /** Replaying from a log file. */
-    REPLAY
-  }
-
-  // Deadband
-  public static final double DEADBAND = 0.1;
-
-  // Robot dimensions and speed
-  public static final double MAX_LINEAR_SPEED = Units.feetToMeters(14.5);
-  public static final double TRACK_WIDTH_X =
-      Units.inchesToMeters(26.5); // X is perpendicular to the front of the robot
-  public static final double TRACK_WIDTH_Y =
-      Units.inchesToMeters(21.5); // Y is parallel to the front of the robot
-  public static final double DRIVE_GEAR_RATIO = (14.0 / 44.0) * (28.0 / 18.0) * (15.0 / 45.0);
-  public static final double TURN_GEAR_RATIO = (24.0 / 8) * (72.0 / 14);
-
-  // Motor and encoder constants
-  public static final int DRIVE_SPARK_MAX_FL = 25;
-  public static final int TURN_SPARK_MAX_FL = 24;
-  public static final int CANCODER_FL = 33;
-  public static final double ABSOLUTE_ENCODER_OFFSET_FL = 0.668;
-
-  public static final int DRIVE_SPARK_MAX_FR = 27;
-  public static final int TURN_SPARK_MAX_FR = 26;
-  public static final int CANCODER_FR = 30;
-  public static final double ABSOLUTE_ENCODER_OFFSET_FR = 0.780 - 0.5;
-
-  public static final int DRIVE_SPARK_MAX_BL = 23;
-  public static final int TURN_SPARK_MAX_BL = 22;
-  public static final int CANCODER_BL = 32;
-  public static final double ABSOLUTE_ENCODER_OFFSET_BL = 0.054;
-
-  public static final int DRIVE_SPARK_MAX_BR = 21;
-  public static final int TURN_SPARK_MAX_BR = 20;
-  public static final int CANCODER_BR = 31;
-  public static final double ABSOLUTE_ENCODER_OFFSET_BR = 0.646;
-
-  // Gyro
-  public static final int PIGEON2_CAN_ID = 26;
-
-  // Odometry
-  public static final double ODOMETRY_FREQUENCY = 250.0;
-  public static final double WHEEL_RADIUS = Units.inchesToMeters(2.0) * 0.9442667069;
+	
+	// Modes
+	public static final Mode currentMode = Mode.REAL;
+	// Deadband
+	public static final double DEADBAND = 0.1;
+	// Robot dimensions and speed
+	public static final double MAX_LINEAR_SPEED = Units.feetToMeters(14.5);
+	public static final double TRACK_WIDTH_X =
+		Units.inchesToMeters(20.25); // X is perpendicular to the front of the robot
+	public static final double TRACK_WIDTH_Y =
+		Units.inchesToMeters(24.5); // Y is parallel to the front of the robot
+	public static final double DRIVE_GEAR_RATIO = (14.0 / 44.0) * (28.0 / 18.0) * (15.0 / 45.0);
+	public static final double TURN_GEAR_RATIO = (24.0 / 8) * (72.0 / 14);
+	// Motor and encoder constants
+	public static final int DRIVE_SPARK_MAX_FL = 24;
+	public static final int TURN_SPARK_MAX_FL = 25;
+	public static final int CANCODER_FL = 33;
+	public static final double ABSOLUTE_ENCODER_OFFSET_FL = 0.668;
+	public static final int DRIVE_SPARK_MAX_FR = 26;
+	public static final int TURN_SPARK_MAX_FR = 27;
+	public static final int CANCODER_FR = 30;
+	public static final double ABSOLUTE_ENCODER_OFFSET_FR = 0.780 - 0.5;
+	public static final int DRIVE_SPARK_MAX_BL = 23;
+	public static final int TURN_SPARK_MAX_BL = 22;
+	public static final int CANCODER_BL = 32;
+	public static final double ABSOLUTE_ENCODER_OFFSET_BL = 0.054;
+	public static final int DRIVE_SPARK_MAX_BR = 21;
+	public static final int TURN_SPARK_MAX_BR = 20;
+	public static final int CANCODER_BR = 31;
+	public static final double ABSOLUTE_ENCODER_OFFSET_BR = 0.646;
+	// Gyro
+	public static final int PIGEON2_CAN_ID = 26;
+	// Odometry
+	public static final double ODOMETRY_FREQUENCY = 250.0;
+	public static final double WHEEL_RADIUS = Units.inchesToMeters(2.0) * 0.9442667069;
+	public enum Mode {
+		/**
+		 * Running on a real robot.
+		 */
+		REAL,
+		
+		/**
+		 * Running a physics simulator.
+		 */
+		SIM,
+		
+		/**
+		 * Replaying from a log file.
+		 */
+		REPLAY
+	}
 }
--- src/main/java/frc/robot/subsystems/drive/GyroIO.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIO.java
@@ -17,14 +17,15 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface GyroIO {
-  default void updateInputs(GyroIOInputs inputs) {}
-
-  @AutoLog
-  class GyroIOInputs {
-    public boolean connected = false;
-    public Rotation2d yawPosition = new Rotation2d();
-    public double[] odometryYawTimestamps = new double[] {};
-    public Rotation2d[] odometryYawPositions = new Rotation2d[] {};
-    public double yawVelocityRadPerSec = 0.0;
-  }
+	default void updateInputs(GyroIOInputs inputs) {
+	}
+	
+	@AutoLog
+	class GyroIOInputs {
+		public boolean connected = false;
+		public Rotation2d yawPosition = new Rotation2d();
+		public double[] odometryYawTimestamps = new double[]{};
+		public Rotation2d[] odometryYawPositions = new Rotation2d[]{};
+		public double yawVelocityRadPerSec = 0.0;
+	}
 }
