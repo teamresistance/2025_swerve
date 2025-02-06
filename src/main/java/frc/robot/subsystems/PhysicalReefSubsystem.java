9dd316e204e94e509ae5ffc020fff316f6998b2e
darkpotatoo <darkpotatoben@gmail.com>
2025-02-05 19:18:04 -0500

    initial physical interface (no real functionality)

---
+++ src/main/java/frc/robot/subsystems/PhysicalReefSubsystem.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PhysicalReefSubsystem extends SubsystemBase {
+  /** Creates a new PhysicalReefSubsystem. */
+  public PhysicalReefSubsystem() {}
+
+  private int level;
+  private int pos;
+  private boolean rl = false; //Left is false, right is true
+
+  public void ChooseReef(int level, int pos, int rl) {
+    if (rl==1) { this.rl = !this.rl; }
+    this.level = level;
+    this.pos = pos;
+    //Drive functiosn to rotate will be added later maybe
+  }
+
+  @Override
+  public void periodic() {
+    
+    // This method will be called once per scheduler run
+  }
+
+}
