337df179cad530d67dcfe7f5c2460c2c072a3aee
darkpotatoo <darkpotatoben@gmail.com>
2025-02-09 15:48:52 -0500

    Removed drive from this branch to enable simulation (it can re-merge later)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,8 +13,6 @@
 
 package frc.robot;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -22,27 +20,16 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commandgroups.ElevatorHighestCommandGroup;
 import frc.robot.commandgroups.ElevatorLowestCommandGroup;
 import frc.robot.commandgroups.ElevatorMiddleCommandGroup;
 import frc.robot.commandgroups.ScoreCommandGroup;
 import frc.robot.commands.ChooseReefCmd;
-import frc.robot.commands.DriveCommands;
 import frc.robot.commands.FlipperReceivingCommand;
-import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.FlipperSubsystem;
 import frc.robot.subsystems.PhysicalReefInterfaceSubsystem;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.drive.GyroIO;
-import frc.robot.subsystems.drive.GyroIOPigeon2;
-import frc.robot.subsystems.drive.ModuleIO;
-import frc.robot.subsystems.drive.ModuleIOSim;
-import frc.robot.subsystems.drive.ModuleIOTalonFX;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -53,10 +40,7 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
  */
 public class RobotContainer {
 
-  private final boolean UseExperimentalPhysicalReefInterface = true;
-
   // Subsystems
-  private final Drive drive;
   private final FlipperSubsystem m_flipperSubsystem = new FlipperSubsystem();
   private final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
 
@@ -68,70 +52,16 @@ public class RobotContainer {
   Transform2d targetTransform = new Transform2d(targetTranslation, targetRotation);
 
   // Controller
-  private final CommandXboxController controller = new CommandXboxController(0);
+  // private final CommandXboxController controller = new CommandXboxController(0);
   private final Joystick Joystick1 = new Joystick(0);
-  private final Joystick Joystick2 = new Joystick(1);
-  private final Joystick CoJoystick = new Joystick(2);
+  // private final Joystick Joystick2 = new Joystick(1);
+  // private final Joystick CoJoystick = new Joystick(2);
 
   // Dashboard inputs
-  private final LoggedDashboardChooser<Command> autoChooser;
+  private LoggedDashboardChooser<Command> autoChooser;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    switch (Constants.currentMode) {
-      case REAL:
-        // Real robot, instantiate hardware IO implementations
-        drive =
-            new Drive(
-                new GyroIOPigeon2(),
-                new ModuleIOTalonFX(TunerConstants.FrontLeft),
-                new ModuleIOTalonFX(TunerConstants.FrontRight),
-                new ModuleIOTalonFX(TunerConstants.BackLeft),
-                new ModuleIOTalonFX(TunerConstants.BackRight));
-        break;
-
-      case SIM:
-        // Sim robot, instantiate physics sim IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIOSim(TunerConstants.FrontLeft),
-                new ModuleIOSim(TunerConstants.FrontRight),
-                new ModuleIOSim(TunerConstants.BackLeft),
-                new ModuleIOSim(TunerConstants.BackRight));
-        break;
-
-      default:
-        // Replayed robot, disable IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
-        break;
-    }
-
-    // Set up auto routines
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
-
-    // Set up SysId routines
-    autoChooser.addOption(
-        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-    autoChooser.addOption(
-        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -148,8 +78,9 @@ public class RobotContainer {
     //
     // Physical reef interface refers to a separate "joystick" which is a few buttons, similar to a
     // macro keyboard
-    if (UseExperimentalPhysicalReefInterface) {
-      final Joystick physicalInterface = new Joystick(1);
+    final boolean UseExperimentalPhyscialReefInterface = true;
+    if (UseExperimentalPhyscialReefInterface) {
+      final Joystick physicalInterface = new Joystick(4);
       final PhysicalReefInterfaceSubsystem m_PhysicalReefSubsystem =
           new PhysicalReefInterfaceSubsystem();
       // execute
@@ -188,54 +119,12 @@ public class RobotContainer {
     new JoystickButton(Joystick1, 3).onTrue(new ElevatorLowestCommandGroup(m_elevatorSubsystem));
     new JoystickButton(Joystick1, 4).onTrue(new ElevatorMiddleCommandGroup(m_elevatorSubsystem));
     new JoystickButton(Joystick1, 6).onTrue(new ElevatorHighestCommandGroup(m_elevatorSubsystem));
-
-    //
-    //    Drive Commands
-    //
-    // Default command, normal field-relative drive
-    drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
-            drive,
-            () -> -applyDeadband(controller.getLeftY()),
-            () -> -applyDeadband(controller.getLeftX()),
-            () -> -applyDeadband(controller.getRightX())));
-
-    // Lock to 0° when A button is held
-    controller
-        .a()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -applyDeadband(controller.getLeftY()),
-                () -> -applyDeadband(controller.getLeftX()),
-                () -> new Rotation2d()));
-
-    // Switch to X pattern when X button is pressed
-    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
-
-    // Reset gyro to 0° when B button is pressed
-    controller
-        .b()
-        .onTrue(
-            Commands.runOnce(
-                    () ->
-                        drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-                    drive)
-                .ignoringDisable(true));
-
-    controller.leftBumper().whileTrue(DriveCommands.goToTransform(drive, targetTransform));
-
-    // **Left Trigger - Go to AprilTag Position A**
-    controller
-        .leftTrigger()
-        .whileTrue(DriveCommands.goTo2DPos(drive, 0.0, 1.0, 0.0)); // Example values
-
-    // **Right Trigger - Go to AprilTag Position B**
-    controller
-        .rightTrigger()
-        .whileTrue(DriveCommands.goTo2DPos(drive, 1.0, 2.0, 0.0)); // Example values
   }
+
+  //
+  //    Drive Commands
+  //
+  // Default command, normal field-relative drive
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
